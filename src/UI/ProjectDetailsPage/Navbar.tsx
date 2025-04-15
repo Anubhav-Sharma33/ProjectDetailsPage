5629e876ff86324239aa3409e162c4fd8b5e81f9
Anubhav Sharma <anubhvsharma33@gmail.com>
2025-04-15 16:35:09 +0530

    third commit

--- src/UI/ProjectDetailsPage/Navbar.tsx
+++ src/UI/ProjectDetailsPage/Navbar.tsx
@@ -8,7 +8,6 @@ const Navbar = () => {
       <div className="w-full px-5 lg:w-[91%] lg:px-3 max-w-[1340px] m-auto flex justify-between">
         <Logo src = {"https://sar.ecis.in/api/starestate/star_estate/projects/2025-02-25_16-33-05_logo.webp"}/>
         <Menu />
-        <Toggler/>
         <Logo src = {"https://www.starestate.com/assets/images/logo-starestate.webp"}/>
       </div>
     </div>
